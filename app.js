a659e86cce11e4982aa8ef61f2e1eac17cdf0c0d
Bharath <bharathanand000@gmail.com>
2018-11-09 16:26:41 +0530

    confusion server with mongoose and REST  part  1

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ const dishRouter = require('./routes/dishRouter');
 const promoRouter = require('./routes/promoRouter');
 const leaderRouter = require('./routes/leaderRouter');
 
+const mongoose = require('mongoose');
+const Dishes = require('./models/dishes');
+const url ='mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) =>{
+  console.log("connected correctly to server !");
+}, (err)=> {console.log(err);});
+
 var app = express();
 
 // view engine setup
